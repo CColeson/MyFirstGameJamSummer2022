502e0bf10224961e601b8b814596293d8f97044e
TheNickster27 <nicholasbarnett5733@gmail.com>
2022-07-16 00:34:28 -0400

    added cool map lols

--- Program/World.cs
+++ Program/World.cs
@@ -4,31 +4,31 @@ using System.Collections.Generic;
 
 public class World : Node2D
 {   
-    private Player Player;
-    private CrewManagerBody CrewManagerBody;
-    private Control CrewManager;
-    public bool CrewMangerIsOpen
-    {
-        get
-        {
-            return CrewManager.Visible;
-        }
-    }
+	private Player Player;
+	private CrewManagerBody CrewManagerBody;
+	private Control CrewManager;
+	public bool CrewMangerIsOpen
+	{
+		get
+		{
+			return CrewManager.Visible;
+		}
+	}
 
-    public override void _Ready()
-    {
-        Player = Game.GetPlayerInstance(this);
-        CrewManager = GetNode<Control>("GUI/CrewMemberManager");
-        //                                          Fuck Godot UI Trees
-        CrewManagerBody = GetNode<CrewManagerBody>("GUI/CrewMemberManager/NinePatchRect/MarginContainer/CrewManagerBody");
-        Player.Connect(nameof(Player.OnPlayerCrewMemberAdded), CrewManagerBody, nameof(CrewManagerBody._OnPlayerCrewAdded));
-    }
+	public override void _Ready()
+	{
+		Player = Game.GetPlayerInstance(this);
+		CrewManager = GetNode<Control>("GUI/CrewMemberManager");
+		//                                          Fuck Godot UI Trees
+		CrewManagerBody = GetNode<CrewManagerBody>("GUI/CrewMemberManager/NinePatchRect/MarginContainer/CrewManagerBody");
+		Player.Connect(nameof(Player.OnPlayerCrewMemberAdded), CrewManagerBody, nameof(CrewManagerBody._OnPlayerCrewAdded));
+	}
 
-    public override void _Process(float delta)
-    {
-        if (Input.IsActionJustPressed("open_crew_member_manager"))
-        {
-            CrewManager.Visible = !CrewManager.Visible;
-        }
-    }
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionJustPressed("open_crew_member_manager"))
+		{
+			CrewManager.Visible = !CrewManager.Visible;
+		}
+	}
 }
